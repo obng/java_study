ccdd3133ca11a006e297137a20cf5b9b345dccf6
bong <p1-02@naver.com>
2025-07-21 17:01:08 +0900

    fix: select, update 기능 추가

--- jdbcProject/src/com/yedam/app/BookDAO.java
+++ jdbcProject/src/com/yedam/app/BookDAO.java
@@ -1,6 +1,7 @@
 package com.yedam.app;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -9,6 +10,66 @@ import java.util.ArrayList;
 // 조회, 등록, 수정, 삭제
 
 public class BookDAO {
+	// 삭제 기능 구현 해보기
+	
+	// 수정. 매개값(int bno, int price), 반환값(boolean)
+	public boolean update(int bno, int price) {
+		Connection conn = DBUtil.getConnect();
+		String query = "update book "
+				     + "set price = ? " 
+				     + "where id = ?";
+		System.out.println(query);
+		
+		try {
+			PreparedStatement stmt = conn.prepareStatement(query);
+			
+			stmt.setInt(1, price);
+			stmt.setInt(2, bno);
+			
+			int r = stmt.executeUpdate();
+			if (r > 0) {
+				return true;
+			}
+		} 
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+	
+	// 등록, 매개값(Book), 반환값(boolean)
+	public boolean insert(Book book) {
+		Connection conn = DBUtil.getConnect();
+		String query = "insert into book (id,"
+									   + "title,"
+									   + "author,"
+									   + "price) "
+				     + "values(?, ?, ?, ?)";
+		System.out.println(query);
+		
+		try {
+			PreparedStatement stmt = conn.prepareStatement(query);
+			
+			stmt.setInt(1, book.getId());
+			stmt.setString(2, book.getTitle());
+			stmt.setString(3, book.getAuthor());
+			stmt.setInt(4, book.getPrice());
+			
+			int r = stmt.executeUpdate();
+			if (r > 0) {
+				return true;
+			}
+		} 
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
 	// 목록 조회 (다건)
 	public ArrayList<Book> findAll() {
 		Connection conn = DBUtil.getConnect();
@@ -24,6 +85,9 @@ public class BookDAO {
 				book.setTitle(rs.getString("title")); // title: title 필드에 할당
 				book.setAuthor(rs.getString("author"));
 				book.setPrice(rs.getInt("price"));
+				
+				// 컬렉션에 추가
+				list.add(book);
 			}
 		}
 		catch (SQLException e) {
--- jdbcProject/src/com/yedam/app/Main.java
+++ jdbcProject/src/com/yedam/app/Main.java
@@ -32,11 +32,55 @@ public class Main {
 				System.out.println("===============================");
 				
 				for (int i=0; i<list.size(); i++) {
-					System.out.println(list.get(i).getId() + "  " 
-									 + list.get(i).getTitle() + "        "
-							         + list.get(i).getAuthor() + "      " 
+					System.out.println(list.get(i).getId() + "\t" 
+									 + list.get(i).getTitle() + "\t"
+							         + list.get(i).getAuthor() + "\t" 
 									 + list.get(i).getPrice());
 				}
+				break;
+				
+			case 2: // 도서 등록  
+				System.out.print("도서번호 > ");
+				int bno = scanner.nextInt();
+				scanner.nextLine();
+				
+				System.out.print("도서제목 > ");
+				String title = scanner.nextLine();
+				
+				System.out.print("도서저자 > ");
+				String author = scanner.nextLine();
+				
+				System.out.print("도서가격 > ");
+				int price = scanner.nextInt();
+				scanner.nextLine();
+				
+				Book book = new Book(bno, title, author, price);
+				
+				if (dao.insert(book)) {
+					System.out.println("정상 등록");
+				}
+				else {
+					System.out.println("등록 중 오류");
+				}
+				
+				break;
+				
+			case 3: 
+				System.out.print("도서번호 > ");
+				bno = scanner.nextInt();
+				scanner.nextLine();
+				
+				System.out.print("도서가격 > ");
+				price = scanner.nextInt();
+				scanner.nextLine();
+				
+				if (dao.update(bno, price)) {
+					System.out.println("정상 등록");
+				}
+				else {
+					System.out.println("등록 중 오류");
+				}
+				
 				break;
 				
 			case 9:
